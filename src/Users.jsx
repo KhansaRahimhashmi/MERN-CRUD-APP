adb09f30456b01b40d42c808b2fb8743fdd59952
Khansa Rahim <khansarahimm@gmail.com>
2023-12-29 23:56:16 +0500

    Frontend Done

--- src/Users.jsx
+++ src/Users.jsx
@@ -27,8 +27,8 @@ function Users() {
                 <td>{user.Email}</td>
                 <td>{user.Age}</td>
                 <td>
-                  <button>Edit</button>
-                  <button>Delete</button>
+                <Link to="/update" className='btn btn-success'> Update</Link>
+                  <button className='btn btn-success' style={{ marginLeft: '8px' }}>Delete</button>
                 </td>
               </tr>
             ))}
