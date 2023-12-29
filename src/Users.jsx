0a6cb1d01478eb4f20245c6c78b3c328008c86d1
Khansa Rahim <khansarahimm@gmail.com>
2023-12-29 23:37:26 +0500

    Set Route for Add button

--- src/Users.jsx
+++ src/Users.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Users() {
   const [users, setUsers] = useState([
@@ -8,6 +9,7 @@ function Users() {
   return (
     <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
       <div className='w-50 bg-white rounded p-3'>
+        <Link to="/create" className='btn btn-success'> Add+ </Link>
         <table className='table'>
           <thead>
             <tr>
@@ -18,7 +20,8 @@ function Users() {
             </tr>
           </thead>
           <tbody>
-            {users.map((user, index) => (
+            {
+            users.map((user, index) => (
               <tr key={index}>
                 <td>{user.Name}</td>
                 <td>{user.Email}</td>
